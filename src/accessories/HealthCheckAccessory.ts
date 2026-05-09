4baecf75e7a91c5f4d2329a1a015bb311e0300b3
Tohar Harush <toahr1010@gmail.com>
2026-05-09 17:57:31 +0300

    Fix | Health Switch

--- src/accessories/HealthCheckAccessory.ts
+++ src/accessories/HealthCheckAccessory.ts
@@ -8,14 +8,14 @@ interface HomekitServices {
 
 export class HealthCheckAccessory {
   private readonly service: Service;
-  private readonly contactState: typeof Characteristic.ContactSensorState;
+  private readonly on: typeof Characteristic.On;
 
   constructor(
     private readonly log: DebugLogger,
     homekit: HomekitServices,
     accessory: PlatformAccessory,
   ) {
-    this.contactState = homekit.Characteristic.ContactSensorState;
+    this.on = homekit.Characteristic.On;
 
     accessory.getService(homekit.Service.AccessoryInformation)!
       .setCharacteristic(homekit.Characteristic.Manufacturer, 'toharush')
@@ -23,14 +23,17 @@ export class HealthCheckAccessory {
       .setCharacteristic(homekit.Characteristic.SerialNumber, 'RA-HEALTH-CHECK');
 
     this.service =
-      accessory.getService(homekit.Service.ContactSensor) ||
-      accessory.addService(homekit.Service.ContactSensor, 'API Health', 'health-check');
+      accessory.getService(homekit.Service.Switch) ||
+      accessory.addService(homekit.Service.Switch, 'API Health', 'health-check');
 
-    this.service.updateCharacteristic(this.contactState, 0);
+    this.service.getCharacteristic(this.on)
+      .onSet(() => undefined);
+
+    this.service.updateCharacteristic(this.on, true);
   }
 
   updateHealth(healthy: boolean): void {
-    this.service.updateCharacteristic(this.contactState, healthy ? 0 : 1);
+    this.service.updateCharacteristic(this.on, healthy);
     if (healthy) {
       this.log.info('Health check: OREF API is reachable');
     } else {
